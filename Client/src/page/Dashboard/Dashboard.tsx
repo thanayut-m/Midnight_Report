df24115bdf0ff83cdedb7af277f37961cc50a45b
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-04-24 02:44:21 +0700

    refactor: add useEffect to update document title

--- Client/src/page/Dashboard/Dashboard.tsx
+++ Client/src/page/Dashboard/Dashboard.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 const Dashboard = () => {
+
+    useEffect(() => {
+        document.title = "Dashboard";
+    }, [])
+
+
     return (
         <div>Dashboard</div>
     )
