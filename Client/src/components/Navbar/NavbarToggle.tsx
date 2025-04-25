29dfa480eea039d945de8394d29240abdfd8cdee
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-04-26 02:50:49 +0700

    refactor(navbar): introduce NavbarProps interface for component props

--- Client/src/components/Navbar/NavbarToggle.tsx
+++ Client/src/components/Navbar/NavbarToggle.tsx
@@ -1,8 +1,12 @@
 import { LuAlignJustify } from "react-icons/lu";
 
+interface NavbarToggleProps {
+    onClick: () => void;
+}
+
 const NavbarToggle = ({
     onClick
-}) => {
+}: NavbarToggleProps) => {
     return (
         <div
             className="text-2xl p-1 mr-2 hover:bg-gray-300 hover:p-1 hover:rounded-sm cursor-pointer"
